830a2fed3e0b2dc4dba249226c10d81835a6672d
Muhammad Olim <58527269+muhammadolim@users.noreply.github.com>
2022-07-13 23:21:43 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    // *** spinner ***
+
+    document.onreadystatechange = function() {
+        if (document.readyState !== "complete") {
+            document.querySelector(
+              "body").style.visibility = "hidden";
+            document.getElementById(
+              "container-spinner").style.visibility = "visible";
+        } else {
+            document.getElementById(
+              "container-spinner").style.display = "none";
+            document.querySelector(
+              "body").style.visibility = "visible";
+        }
+    };
+
+
     // *** sorting cards animation setup ***
 
     var mixer = mixitup('.projects', {
